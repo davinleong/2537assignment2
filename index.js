ab14070e690c1873f94bce50a65109693346042d
Davin Leong <davinleongg@gmail.com>
2023-04-25 15:13:51 -0700

    added email and user pages

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+//Users and Passwords (in memory 'database')
+var users = []; 
+
+
+app.use(express.urlencoded({extended: false}));
+
 app.get('/', (req,res) => {
     res.send("<h1>Hello World!</h1>");
 });
@@ -13,6 +19,66 @@ app.get('/about', (req,res) => {
 
     res.send("<h1 style='color:"+color+";'>Davin</h1>");
 });
+
+app.get('/contact', (req,res) => {
+  var missingEmail = req.query.missing;
+  var html = `
+      email address:
+      <form action='/submitEmail' method='post'>
+          <input name='email' type='text' placeholder='email'>
+          <button>Submit</button>
+      </form>
+  `;
+  if (missingEmail) {
+      html += "<br> email is required";
+  }
+  res.send(html);
+});
+
+app.post('/submitEmail', (req,res) => {
+  var email = req.body.email;
+  if (!email) {
+      res.redirect('/contact?missing=1');
+  }
+  else {
+      res.send("Thanks for subscribing with your email: "+email);
+  }
+});
+
+app.get('/createUser', (req,res) => {
+  var html = `
+  <form action='/submitUser' method='post'>
+  <input name='username' type='text' placeholder='username'>
+  <input name='password' type='password' placeholder='password'>
+  <button>Submit</button>
+  </form>
+  `;
+  res.send(html);
+});
+
+app.post('/submitUser', (req,res) => {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  users.push({ username: username, password: password });
+
+  console.log(users);
+
+  var usershtml = "";
+  for (i = 0; i < users.length; i++) {
+      usershtml += "<li>" + users[i].username + ": " + users[i].password + "</li>";
+  }
+
+  var html = "<ul>" + usershtml + "</ul>";
+  res.send(html);
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("*", (req,res) => {
+	res.status(404);
+	res.send("Page not found - 404");
+})
 
 app.listen(port, () => {
 	console.log("Node application listening on port "+port);
